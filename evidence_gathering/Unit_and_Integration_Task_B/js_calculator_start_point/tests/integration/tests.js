aa64596fb6ecbdc9f16763a8a839913d5c077f82
Nyalls Hemingway <hemingwaynyalls@gmail.com>
2018-11-18 19:05:38 +0000

    got concat function working

--- evidence_gathering/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ evidence_gathering/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -17,7 +17,10 @@ describe('calculator functionality', function() {
   })
 
   it("should concatenate multiple number button clicks", function(){
-
+    running_total = element(by.css("#running_total"))
+    element(by.css("#number2")).click();
+    element(by.css("#number4")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("24")
   })
 
   it("should chain multiple operations together", function() {
@@ -25,10 +28,9 @@ describe('calculator functionality', function() {
   })
 
   it("should be able to clear the running total without affecting the calculation", function() {
-    
+
   })
 
-  // - `calculator.numberClick()` - concatenate multiple number button clicks
   // - `calculator.operatorClick()` - chain multiple operations together
   // - `calculator.clearClick()` - clear the running total without affecting the calculation
 
